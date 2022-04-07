65f231a3f50abf3537b179ec51d63e22670bd1cd
eitan lev <eitanleviev111@gmail.com>
2022-04-07 14:27:08 +0300

    dragging

---
+++ huji hackathon 22 unity/Assets/Eitan/scripts/DragContact.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragContact : MonoBehaviour
+{
+    private bool moveAllowed;
+    private Collider2D col;
+
+    private void Awake()
+    {
+        col = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            
+            if (touch.phase == TouchPhase.Began)
+            {
+                Collider2D touchColl = Physics2D.OverlapPoint(touchPosition);
+                if (col == touchColl)
+                {
+                    moveAllowed = true;
+                }
+            }
+            
+            if (touch.phase == TouchPhase.Moved)
+            {
+                if (moveAllowed)
+                {
+                    transform.position = new Vector2(touchPosition.x, touchPosition.y);
+                }
+            }
+            
+            if (touch.phase == TouchPhase.Ended)
+            {
+                moveAllowed = false;
+            }
+            
+        }
+
+        
+    }
+}
