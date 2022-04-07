7d3e7a70204a4aded4f671d7cd9a0626ae4886b2
Ethan Glick <ethan.glick@cs.huji.ac.il>
2022-04-07 14:47:29 +0300

    added contact classes

--- huji hackathon 22 unity/Assets/Team/Ethan/Communicator.cs
+++ huji hackathon 22 unity/Assets/Team/Ethan/Communicator.cs
@@ -15,13 +15,31 @@ namespace Team.Ethan
         
         private DateTime _lastComDate;
         private ComTypes _comType;
-        private int _interval;
+        private int _interval; // in days. -1 means never.
         
-        public Communicator(ComTypes comType, int interval)
+        public Communicator(ComTypes comType, int interval=0)
         {
             _lastComDate = DateTime.Now;
             _comType = comType;
             _interval = interval;
+        }
+
+        /**
+         * returns days until next time to communicate.
+         * if days is less than 0, returns 0.
+         * if interval set to 0 (never), returns -1.
+         */
+        public int DaysToNextInterval()
+        {
+            if (_interval == 0)
+                return -1;
+            
+            TimeSpan interval = new TimeSpan(_interval*24);
+            DateTime next = _lastComDate.Add(interval);
+
+            int daysToGo = next.Subtract(DateTime.Now).Days;
+       
+            return daysToGo > 0 ? daysToGo : 0;
         }
 
         public void UpdateComDate()
