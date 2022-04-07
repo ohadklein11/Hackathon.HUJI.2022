d553ee6c33cbdbba8e809b9fe07d124404ebe085
Ethan Glick <ethan.glick@cs.huji.ac.il>
2022-04-07 22:06:54 +0300

    Added isOnline notification

--- huji hackathon 22 unity/Assets/Team/Ethan/NotificationController.cs
+++ huji hackathon 22 unity/Assets/Team/Ethan/NotificationController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.Notifications.Android;
 
 namespace Team.Ethan
 {
@@ -22,7 +23,31 @@ namespace Team.Ethan
 
         private static void NotifyUser(Contact contact)
         {
+            // remove notifications that have already been displayed
+            AndroidNotificationCenter.CancelAllDisplayedNotifications();
             
+            var channel = new AndroidNotificationChannel()
+            {
+                Id = contact.Name,
+                Name = "Notification Channel",
+                Importance = Importance.Default,
+                Description = "Reminder notifications",
+            };
+            AndroidNotificationCenter.RegisterNotificationChannel(channel);
+
+            var notification = new AndroidNotification();
+            notification.Title = contact.Name + " is online!";
+            notification.Text = "Tap to contact them!";
+            notification.FireTime = System.DateTime.Now;
+
+            var id = AndroidNotificationCenter.SendNotification(notification, contact.Name);
+
+            // if the script is run and a message is already scheduled, cancel it and re-schedule another message
+            if (AndroidNotificationCenter.CheckScheduledNotificationStatus(id) == NotificationStatus.Scheduled)
+            {
+                AndroidNotificationCenter.CancelAllNotifications();
+                AndroidNotificationCenter.SendNotification(notification, contact.Name);
+            }
         }
     }
 }
